45b3e6ea3b9823e7a509f100c4c82006589b6112
Gaurav Singh <72642688+sgaurav37533@users.noreply.github.com>
2022-11-27 23:12:27 +0530

    Progress in Data Pipeline

--- dags/astro_orders.py
+++ dags/astro_orders.py
@@ -1,30 +1,45 @@
 from datetime import datetime
 
 from airflow.models import DAG
-from pandas import DataFrame 
+from pandas import DataFrame
 
 from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table
 
-S3_FILE_PATH ="s3://gaurav-airflowpipeline/orders_data_header.csv"
-S3_CONN_ID="aws_default"
-SNOWFLAKE_CONN_ID="snowflake_default"
-SNOWFLAKE_ORDERS="order_table"
-SNOWFLAKE_FILTERED_ORDERS ="filtered_table"
-SNOWFLAKE_JOINED="joined_table"
-SNOWFLAKE_CUSTOMERS="customers_table"
-SNOWFLAKE_REPORTING="reporting_table"
+S3_FILE_PATH = "s3://gaurav-airflowpipeline"
+S3_CONN_ID = "aws_default"
+SNOWFLAKE_CONN_ID = "snowflake_default"
+SNOWFLAKE_ORDERS = "order_table"
+SNOWFLAKE_FILTERED_ORDERS = "filtered_table"
+SNOWFLAKE_JOINED = "joined_table"
+SNOWFLAKE_CUSTOMERS = "customers_table"
+SNOWFLAKE_REPORTING = "reporting_table"
 
-with DAG(dag_id='astro_orders', start_date=datetime(2020,1,1), schedule='@daily', catchup=False):
-    orders_data=aql.load_file(
+
+@aql.transform
+def filter_orders(input_table: Table):
+    return "SELECT * FROM{{input_table}} WHERE amount > 150"
+
+
+@aql.transform
+def join_orders_customers(input_table: Table):
+    return """SELECT c.customer_id, customer_name, order_id, purchase_date, amount, type
+    FROM {{filtered_orders_table}} f JOIN {{customers_table}} c ON f.customer_id = c.customer_id"""
+
+
+with DAG(dag_id='astro_orders', start_date=datetime(2020, 1, 1), schedule='@daily', catchup=False):
+    orders_data = aql.load_file(
         input_file=File(
-            path=S3_FILE_PATH+"/orders_data_header.csv" ,conn_id=S3_CONN_ID
+            path=S3_FILE_PATH+"/orders_data_header.csv", conn_id=S3_CONN_ID
         ),
         output_table=Table(conn_id=SNOWFLAKE_CONN_ID)
     )
 
-    customers_table=Table(
+    customers_table = Table(
         name=SNOWFLAKE_CUSTOMERS,
         conn_id=SNOWFLAKE_CONN_ID,
     )
+
+    joined_data = join_orders_customers(
+        filter_orders(orders_data), customers_table)
